b96605bfe634adf490619514c0637b8159966abc
Alexandre Gaudencio <shahor@shahor.fr>
2019-08-19 18:03:11 +0200

    use apollo server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require("graphql-yoga")
+const { ApolloServer, gql } = require("apollo-server")
 const { withFilter } = require("graphql-subscriptions")
 const { RedisPubSub } = require("graphql-redis-subscriptions")
 
@@ -6,51 +6,59 @@ const CHANNEL = "noteCreated"
 
 const pubsub = new RedisPubSub()
 
-const typeDefs = `
-	type Query {
-		hello: String!
-	}
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
 
-	type Note {
-		id: ID!
-		createdAt: String!
-		createdBy: ID!
-		updatedAt: String!
-		title: String!
-		channelId: ID!
-		organizationId: ID!
-		pinnedAt: String
-		pinnedBy: String
-		archivedAt: String
-		archivedBy: ID
-		activityCount: Int
-		lastActivityAt: String
-		lastActivityBy: ID
-		lastActivityType: String!
-		collections: [ID!]!
-		publicShareToken: String
-		publiclySharedAt: String
-		publiclySharedBy: String
-	}
+  type Note {
+    id: ID!
+    createdAt: String!
+    createdBy: ID!
+    updatedAt: String!
+    title: String!
+    channelId: ID!
+    organizationId: ID!
+    pinnedAt: String
+    pinnedBy: String
+    archivedAt: String
+    archivedBy: ID
+    activityCount: Int
+    lastActivityAt: String
+    lastActivityBy: ID
+    lastActivityType: String!
+    collections: [ID!]!
+    publicShareToken: String
+    publiclySharedAt: String
+    publiclySharedBy: String
+  }
 
-	type Subscription {
-		noteCreated: Note
-	}
+  type Subscription {
+    noteCreated: Note
+  }
 `
-const resolvers = {
-    Subscription: {
-        noteCreated: {
-            subscribe: withFilter(
-                function() {
-                    return pubsub.asyncIterator(CHANNEL)
-                },
-                function filterFn() {
-                    return true
-                }
-            ),
-        },
-    },
-}
-const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } })
 
-server.start(() => console.log("Server is running on localhost:4000"))
+const resolvers = {
+  Subscription: {
+    noteCreated: {
+      subscribe: withFilter(
+        function() {
+          return pubsub.asyncIterator(CHANNEL)
+        },
+        function filterFn() {
+          return true
+        }
+      ),
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  subscriptions: {
+    path: "/",
+  },
+})
+
+server.listen().then(() => console.log("Server is running on localhost:4000"))
